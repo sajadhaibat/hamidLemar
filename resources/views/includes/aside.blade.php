3a8a43600f552304a47770274366c6080eb77b27
Sajad Haibat <s.haibat@pomtech.com>
2019-10-07 14:14:51 +0430

    Aside Modification

--- resources/views/includes/aside.blade.php
+++ resources/views/includes/aside.blade.php
@@ -380,6 +380,14 @@
                 </li>
                 <li class="header">MAIN</li>
                 <li class="active open"> <a href="index.html"><i class="zmdi zmdi-home"></i><span>Dashboard</span></a></li>
+                <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-delicious"></i><span>Customers</span>
+                        </a>
+                    <ul class="ml-menu">
+                        <li><a href="{{route('customers.create')}}">Add Customer</a></li>
+                        <li><a href="{{route('customers.index')}}">Customers List</a></li>
+                        <li><a href="widgets-chart.html">Customers Payment</a></li>
+                    </ul>
+                </li>
                 <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-apps"></i><span>App</span> <span class="badge badge-success float-right">7</span></a>
                     <ul class="ml-menu">
                         <li><a href="mail-inbox.html">Inbox</a></li>
@@ -391,104 +399,7 @@
                         <li><a href="app-ticket.html">Support Ticket</a></li>
                     </ul>
                 </li>
-                <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-swap-alt"></i><span>UI Elements</span> <span class="badge badge-default float-right">15</span></a>
-                    <ul class="ml-menu">
-                        <li><a href="ui_kit.html">UI KIT</a></li>
-                        <li><a href="alerts.html">Alerts</a></li>
-                        <li><a href="collapse.html">Collapse</a></li>
-                        <li><a href="colors.html">Colors</a></li>
-                        <li><a href="dialogs.html">Dialogs</a></li>
-                        <li><a href="icons.html">Icons</a></li>
-                        <li><a href="list-group.html">List Group</a></li>
-                        <li><a href="media-object.html">Media Object</a></li>
-                        <li><a href="modals.html">Modals</a></li>
-                        <li><a href="notifications.html">Notifications</a></li>
-                        <li><a href="progressbars.html">Progress Bars</a></li>
-                        <li><a href="range-sliders.html">Range Sliders</a></li>
-                        <li><a href="sortable-nestable.html">Sortable & Nestable</a></li>
-                        <li><a href="tabs.html">Tabs</a></li>
-                        <li><a href="waves.html">Waves</a></li>
-                    </ul>
-                </li>
-                <li class="header">FORMS, CHARTS, TABLES</li>
-                <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-assignment"></i><span>Forms</span> <span class="badge badge-warning float-right">9</span></a>
-                    <ul class="ml-menu">
-                        <li><a href="basic-form-elements.html">Basic Elements</a></li>
-                        <li><a href="advanced-form-elements.html">Advanced Elements</a></li>
-                        <li><a href="form-examples.html">Form Examples</a></li>
-                        <li><a href="form-validation.html">Form Validation</a></li>
-                        <li><a href="form-wizard.html">Form Wizard</a></li>
-                        <li><a href="form-editors.html">Editors</a></li>
-                        <li><a href="form-upload.html">File Upload</a></li>
-                        <li><a href="form-img-cropper.html">Image Cropper</a></li>
-                        <li><a href="form-summernote.html">Summernote</a></li>
-                    </ul>
-                </li>
-                <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-grid"></i><span>Tables</span> <span class="badge badge-info float-right">6</span></a>
-                    <ul class="ml-menu">
-                        <li><a href="normal-tables.html">Normal Tables</a></li>
-                        <li><a href="jquery-datatable.html">Jquery Datatables</a></li>
-                        <li><a href="editable-table.html">Editable Tables</a></li>
 
-                        <li><a href="table-color.html">Tables Color</a></li>
-                        <li><a href="table-filter.html">Tables Filter</a></li>
-                    </ul>
-                </li>
-                <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-chart"></i><span>Charts</span> <span class="badge badge-default float-right">6</span></a>
-                    <ul class="ml-menu">
-                        <li><a href="morris.html">Morris</a></li>
-                        <li><a href="flot.html">Flot</a></li>
-                        <li><a href="chartjs.html">ChartJS</a></li>
-                        <li><a href="sparkline.html">Sparkline</a></li>
-                        <li><a href="jquery-knob.html">Jquery Knob</a></li>
-                        <li><a href="chart-e.html">E Chart</a></li>
-                    </ul>
-                </li>
-                <li class="header">EXTRA COMPONENTS</li>
-                <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-delicious"></i><span>Customers</span>
-                        </a>
-                    <ul class="ml-menu">
-                        <li><a href="{{route('customers.create')}}">Add Customer</a></li>
-                        <li><a href="{{route('customers.index')}}">Customers List</a></li>
-                        <li><a href="widgets-chart.html">Customers Payment</a></li>
-                    </ul>
-                </li>
-                <li class="sm_menu_btm"> <a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-lock"></i><span>Authentication</span> <span class="badge badge-default float-right">9</span></a>
-                    <ul class="ml-menu">
-                        <li><a href="sign-in.html">Sign In</a></li>
-                        <li><a href="sign-up.html">Sign Up</a></li>
-                        <li><a href="forgot-password.html">Forgot Password</a></li>
-                        <li><a href="404.html">Page 404</a></li>
-                        <li><a href="403.html">Page 403</a></li>
-                        <li><a href="500.html">Page 500</a></li>
-                        <li><a href="503.html">Page 503</a></li>
-                        <li><a href="page-offline.html">Page Offline</a></li>
-                        <li><a href="locked.html">Locked Screen</a></li>
-                    </ul>
-                </li>
-                <li class="sm_menu_btm"> <a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-copy"></i><span>Sample Pages</span> <span class="badge badge-primary float-right">12</span></a>
-                    <ul class="ml-menu">
-                        <li><a href="blank.html">Blank Page</a></li>
-                        <li><a href="teams-board.html">Teams Board</a></li>
-                        <li><a href="projects.html">Projects List</a></li>
-                        <li><a href="image-gallery.html">Image Gallery</a></li>
-                        <li><a href="profile.html">Profile</a></li>
-                        <li><a href="timeline.html">Timeline</a></li>
-                        <li><a href="horizontal-timeline.html">Horizontal Timeline</a></li>
-                        <li><a href="pricing.html">Pricing</a></li>
-                        <li><a href="invoices.html">Invoices</a></li>
-                        <li><a href="faqs.html">FAQs</a></li>
-                        <li><a href="search-results.html">Search Results</a></li>
-                        <li><a href="helper-class.html">Helper Classes</a></li>
-                    </ul>
-                </li>
-                <li class="sm_menu_btm"> <a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-map"></i><span>Maps</span> <span class="badge badge-default float-right">3</span></a>
-                    <ul class="ml-menu">
-                        <li><a href="google.html">Google Map</a></li>
-                        <li><a href="yandex.html">YandexMap</a></li>
-                        <li><a href="jvectormap.html">jVectorMap</a></li>
-                    </ul>
-                </li>
             </ul>
         </div>
     </div>
